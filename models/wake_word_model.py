11be7c69553fbb78d2506666cd83a9610d646b86
Dominik Hec <dhec@student.agh.edu.pl>
2026-06-10 22:05:25 +0200

    Added CRNN model

--- models/wake_word_model.py
+++ models/wake_word_model.py
@@ -17,7 +17,7 @@ class SimpleCNN(nn.Module):
         self.pool = nn.MaxPool2d(2)
 
         self.fc1 = nn.Linear(2688, 64)  # Adjust the input size based on your actual input dimensions
-        self.fc2 = nn.Linear(64, 3)
+        self.fc2 = nn.Linear(64, 2)
 
     def forward(self, x):
 
@@ -30,4 +30,45 @@ class SimpleCNN(nn.Module):
         x = self.fc2(x)
 
         return x
+    
+
+class CRNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.bilstm1 = nn.LSTM(256 * 8, 256, bidirectional=True, batch_first=True)
+        self.bilstm2 = nn.LSTM(512, 256, bidirectional=True, batch_first=True)
+        self.fc = nn.Linear(512, 2)
+
+    def forward(self, x):
+        x = self.features(x)
+        B, C, H, W = x.shape
+        x = x.permute(0, 3, 2, 1).contiguous().view(B, W, C * H)
+        
+        x, _ = self.bilstm1(x)
+        x, _ = self.bilstm2(x)
+        
+        x = x.mean(dim=1) 
+        return self.fc(x)
+
 
